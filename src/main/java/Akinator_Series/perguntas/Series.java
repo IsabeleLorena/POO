7c31d651908979caf17cad1e7a8f6fe9ea2cc3a5
isabele lorena <isabele.lorena@ibm.com>
2023-11-07 11:37:24 -0300

    arrumando estrutura do projeto

---
+++ src/main/java/Akinator_Series/perguntas/Series.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Akinator_Series.perguntas;
+
+/**
+ *
+ * @author Dieuphenson
+ */
+class Series {
+
+    Series(String em_qual_país_essa_serie_está_ambientada_, String reinu_Unido, String espanha, String frança, String brasil, String usa) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
