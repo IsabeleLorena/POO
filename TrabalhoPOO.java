1cebfbe44c17867b935b8aaccc6e72ac1d2934cd
Dieuphenson Jean <147110592+Dieuphenson@users.noreply.github.com>
2023-11-07 05:07:41 -0300

    Add files via upload

---
+++ TrabalhoPOO.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.dobackaofront.trabalhopoo;
+
+/**
+ *
+ * @author Dieuphenson
+ */
+public class TrabalhoPOO {
+
+    public static void main(String[] args) {
+        JogoSerie serie = new JogoSerie();
+        serie.iniciarPergunta();
+    }
+}
